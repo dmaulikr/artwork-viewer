7d698474651eee97a472c12618d4a3c59fe5e4bb
konojunya <junya03dance@gmail.com>
2017-06-22 14:22:42 +0900

    customセルにセットする関数の実装

--- artwork-viewer/ViewController.swift
+++ artwork-viewer/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var artistData:[[String:String]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.fetchArtistData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func fetchArtistData(){
         let params = [
             "term": "AAA",
             "media": "music",
@@ -25,18 +41,67 @@ class ViewController: UIViewController {
         let ra = RequestAgent(url: "https://itunes.apple.com", method: "get", path: "/search", data: params)
         ra.run(
             success: { json in
-                print(json)
+                let results = json["results"] as! [[String:String]]
+                for result in results.enumerated() {
+                    self.artistData.append([
+                        "trackName": result.element["trackName"]!,
+                        "artistName": result.element["artistName"]!,
+                        "artworkUrl": result.element["artworkUrl100"]!
+                    ])
+                }
             },
             fail: { error in
                 print(error)
             }
         )
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.artistData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        
+        cell.setCell(self.artistData[indexPath.row])
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("hoge")
     }
 
+}
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var artworkImage: UIImageView!
+    @IBOutlet weak var musicName: UILabel!
+    @IBOutlet weak var artistName: UILabel!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setCell(_ data: [String:String]){
+        let url = URL(string: data["artworkUrl"]!)
+        guard let imageData = try? Data(contentsOf: url!) else { return }
+        
+        self.artworkImage.image = UIImage(data: imageData)
+        self.artistName.text = data["artistName"]
+        self.musicName.text = data["trackName"]
+    }
+    
 }
 
