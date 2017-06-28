55275fe10739fd0b7267ef7a6017972810ef5cb8
konojunya <junya03dance@gmail.com>
2017-06-29 02:04:40 +0900

    customCellを外部ファイルへ

--- artwork-viewer/ViewController.swift
+++ artwork-viewer/ViewController.swift
@@ -97,39 +97,3 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
 }
-
-class CustomTableViewCell: UITableViewCell {
-    
-    @IBOutlet weak var artworkImage: UIImageView!
-    @IBOutlet weak var musicName: UILabel!
-    @IBOutlet weak var artistName: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-    func setCell(_ model: ArtworkModel){
-        
-        self.artistName.text = model.artistName
-        self.musicName.text = model.musicName
-        
-        let url = URL(string: model.artworkImage)
-        
-        self.artworkImage.image = UIImage(named: "placeholder")
-        
-        DispatchQueue.global(qos: .background).async {
-            let data = try? Data(contentsOf: url!)
-            DispatchQueue.main.async {
-                if let imageData = data {
-                    self.artworkImage.image = UIImage(data: imageData)
-                }
-            }
-        }
-    }
-    
-}
-
